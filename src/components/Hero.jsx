bf37ffaf5325e61d1b7e7c69028573a26eb0bc22
iafabianalvarezmo <iafabianalvarezmo@gmail.com>
2025-07-22 04:14:02 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         <img  
           alt="Banda de música del recuerdo"
           className="w-full h-full object-cover sepia-filter"
-         src="public/fondo.jpg" />
+         src="/public/fondo.jpg" />
         <div className="absolute inset-0 vintage-gradient opacity-80"></div>
       </div>
 
