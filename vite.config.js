48281eec5a8621f582b186c595ce50e5ea51c62e
iafabianalvarezmo <iafabianalvarezmo@gmail.com>
2025-07-22 04:00:17 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // <-- AÑADIR ESTA LÍNEA
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // AÑADIR TODA ESTA SECCIÓN 'resolve'
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
